3cdd892d9166ef23d63392ac21fad29d7901c22b
Gabriel Moretto <gabrielm@esgn.com.br>
2024-12-25 13:05:12 -0300

    spots filters

---
+++ src/routes/spots/spots-filters.tsx
@@ -0,0 +1,120 @@
+import { CountrySelect, StateSelect } from "@/components/ui/country-select";
+import { DualRangeSlider } from "@/components/ui/dual-range-slider";
+import { Input } from "@/components/ui/input";
+import { MultiSelect } from "@/components/ui/multi-select";
+import { SpotType } from "@/types/spots";
+import { useState } from "react";
+
+export type SpotsFiltersType = {
+    search?: string;
+    types?: string[];
+    distance?: number[];
+    address?: {
+        country?: string;
+        state?: string;
+        city?: string;
+    };
+    rating?: number[];
+}
+
+interface SpotsFiltersProps {
+    filters: SpotsFiltersType;
+    onFiltersChange: (filters: SpotsFiltersType) => void;
+}
+
+export function SpotsFilters(props: SpotsFiltersProps) {
+    const [locationFilterType, setLocationFilterType] = useState<"distance" | "address">("distance");
+
+    return (
+        <div className="flex flex-col w-full">
+            <h3 className="text-sm font-medium mb-2">
+                Filters
+            </h3>
+            
+            <div className="flex justify-between gap-2">
+                <Input
+                    placeholder="Search for spots"
+                    className="h-10"
+                />
+            </div>
+
+            <div className="flex flex-col mt-6">
+                <h3 className="text-xs">
+                    Types
+                </h3>
+
+                <MultiSelect
+                    options={Object.entries(SpotType).map(([key, value]) => ({ label: key, value }))}
+                    placeholder="Select types"
+                    defaultValue={props.filters.types}
+                    onValueChange={(types) => props.onFiltersChange({...props.filters, types})}
+                    className="focus:bg-popover hover:bg-popover mt-2 font-normal bg-popover"
+                />
+            </div>
+
+            <div className="flex flex-col mt-6">
+                <div className="w-full flex justify-between">
+                    <h3
+                        className="text-xs"
+                    >
+                        {locationFilterType === "distance" ? "Distance" : "Address"}
+                    </h3>
+                    <h3 
+                        onClick={() => setLocationFilterType(locationFilterType === "distance" ? "address" : "distance")}
+                        className="text-xs hover:cursor-pointer hover:underline hover:text-secondary duration-100"
+                    >
+                        Filter by {locationFilterType === "distance" ? "address" : "distance"}
+                    </h3>
+                </div>
+
+                {
+                    locationFilterType === "address" ? (
+                        <div className="flex flex-col mt-2 gap-2">
+                            <CountrySelect
+                                value={props.filters.address?.country}
+                                onChange={(country) => props.onFiltersChange({...props.filters, address: { ...props.filters.address, country, state: undefined }})}
+                            />
+                            <StateSelect
+                                disabled={!props.filters.address?.country}
+                                country={props.filters.address?.country}
+                                value={props.filters.address?.state}
+                                onChange={(state) => props.onFiltersChange({...props.filters, address: { ...props.filters.address, state }})}
+                            />
+                            <Input
+                                placeholder="City"
+                                className="h-10"
+                                disabled={!props.filters.address?.state}
+                            />
+                        </div>
+                    ) : (
+                        <DualRangeSlider
+                            label={(value) => <span className="text-xs mt-10">{value}km</span>}
+                            value={props.filters.distance}
+                            onValueChange={(distance) => props.onFiltersChange({...props.filters, distance})}
+                            min={0}
+                            max={100}
+                            step={1}
+                            className="text-sm mt-2"
+                        />
+                    )
+                }
+            </div>
+
+            <div className="flex flex-col mt-6">
+                <h3 className="text-xs">
+                    Rating
+                </h3>
+
+                <DualRangeSlider
+                    label={(value) => <span className="text-xs mt-10">{value}</span>}
+                    value={props.filters.rating}
+                    onValueChange={(rating) => props.onFiltersChange({...props.filters, rating})}
+                    min={0}
+                    max={10}
+                    step={0.1}
+                    className="text-sm mt-2"
+                />
+            </div>
+        </div>
+    );
+}
